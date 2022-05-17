e86f78659579a3c0b95bf912b861522bf0404415
Ville Toimela <ville.toimela@hotmail.com>
2022-05-17 10:10:33 +0000

    Dev 12

--- components/works-item.tsx
+++ components/works-item.tsx
@@ -45,8 +45,8 @@ export function WorksItem({
             quality={100}
           />
         </div>
-        <div className="mx-auto flex cursor-pointer items-center justify-center text-sm  text-white underline">
-          <Link href={link}>linkki repoon || sivulle</Link>
+        <div className="mx-auto mt-5 flex cursor-pointer items-center justify-center text-sm  text-white underline underline-offset-4">
+          <Link href={link}>linkki demoon || sivulle</Link>
         </div>
       </div>
     </div>
