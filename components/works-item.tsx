564b2e91d0dc1910b75fbcbe58b034c4884444b8
villetoimela <ville.toimela@hiisi.digital>
2022-09-29 11:41:25 +0300

    Added button for "live || repo" link in WorksItem

--- components/works-item.tsx
+++ components/works-item.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import { Button1 } from "./button1"
 
 /* eslint-disable-next-line */
 export interface WorksItemProps {
@@ -10,6 +11,7 @@ export interface WorksItemProps {
   imgHeight: number
   tag1?: string
   link: string
+  btnClassName?: string
 }
 
 export function WorksItem({
@@ -20,6 +22,7 @@ export function WorksItem({
   imgHeight,
   tag1,
   link,
+  btnClassName,
 }: WorksItemProps) {
   return (
     <div className="mb-10 flex w-full flex-col bg-black bg-opacity-30 font-Syne md:flex-row">
@@ -33,15 +36,14 @@ export function WorksItem({
             {tag1}
           </p>
         </div>
+        <a href={link} className={` ${btnClassName} w-40 my-4 px-6 py-2 text-center font-bold text-sm text-white border-2 border-red-300 rounded-md hover:bg-red-300 duration-200`}>LIVE || REPO</a>
         <div className=" max-w-4xl border border-white sm:mb-20"></div>
       </div>
       <div className=" w-full self-center md:w-1/2 ">
         <div className="relative h-64 overflow-y-auto p-4 duration-300 scrollbar-hide hover:scale-110 sm:h-[19rem]">
           <Image src={imgSrc} width={imgWidth} height={imgHeight} />
         </div>
-        <div className="mx-auto mt-5 flex items-center justify-center gap-5 text-sm text-white">
-          <Link href={link}>Repo || Live</Link>
-        </div>
+        
       </div>
     </div>
   )
