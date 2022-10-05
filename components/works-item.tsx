08cfc141505d2c1defc9d324b2f47624b5a789a2
villetoimela <ville.toimela@hiisi.digital>
2022-10-05 09:08:08 +0300

    Moved "live or repo" button under portfolio project image

--- components/works-item.tsx
+++ components/works-item.tsx
@@ -36,14 +36,15 @@ export function WorksItem({
             {tag1}
           </p>
         </div>
-        <a href={link} className={` ${btnClassName} w-40 my-4 px-6 py-2 text-center font-bold text-sm text-white border-2 border-red-300 rounded-md hover:bg-red-300 duration-200`}>LIVE || REPO</a>
         <div className=" max-w-4xl border border-white sm:mb-20"></div>
       </div>
       <div className=" w-full self-center md:w-1/2 ">
         <div className="relative h-64 overflow-y-auto p-4 duration-300 scrollbar-hide hover:scale-110 sm:h-[19rem]">
           <Image src={imgSrc} width={imgWidth} height={imgHeight} />
         </div>
-        
+        <button className="flex justify-center items-center mx-auto">
+        <a href={link} className={` ${btnClassName} w-full my-4 px-6 py-2 text-center font-bold text-sm text-white border-2 border-red-300 rounded-md hover:bg-red-300 duration-200 z-50 `}>LIVE || REPO</a>
+        </button>
       </div>
     </div>
   )
