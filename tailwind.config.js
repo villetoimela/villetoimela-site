64bdb2e3a0b2e3dabea12b1963b483779c04ccbd
villetoimela <ville.toimela@hiisi.digital>
2022-10-05 10:39:37 +0300

    prettier things :D deleted spaces or something like that

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,66 +7,66 @@ module.exports = {
     extend: {
       fontFamily: {
         Syne: ["Syne"],
-       },
+      },
 
       keyframes: {
         bgAnimation: {
-          "0%": { 
-            transform: "translate(0,0)" 
+          "0%": {
+            transform: "translate(0,0)"
           },
-          "10%": { 
-            transform: "translate(-5%,-5%)" 
+          "10%": {
+            transform: "translate(-5%,-5%)"
           },
-          "20%": { 
-            transform: "translate(-10%,5%)" 
+          "20%": {
+            transform: "translate(-10%,5%)"
           },
-          "30%": { 
-            transform: "translate(5%,-10%)" 
+          "30%": {
+            transform: "translate(5%,-10%)"
           },
-          "40%": { 
-            transform: "translate(-5%,15%)" 
+          "40%": {
+            transform: "translate(-5%,15%)"
           },
-          "50%": { 
-            transform: "translate(-10%,5%)" 
+          "50%": {
+            transform: "translate(-10%,5%)"
           },
-          "60%": { 
-            transform: "translate(15%,0)" 
+          "60%": {
+            transform: "translate(15%,0)"
           },
-          "70%": { 
-            transform: "translate(0,10%)" 
+          "70%": {
+            transform: "translate(0,10%)"
           },
-          "80%": { 
-            transform: "translate(-15%,0)" 
+          "80%": {
+            transform: "translate(-15%,0)"
           },
-          "90%": { 
-            transform: "translate(10%,5%)" 
+          "90%": {
+            transform: "translate(10%,5%)"
           },
-          "100%": { 
-            transform: "translate(5%,0)" 
+          "100%": {
+            transform: "translate(5%,0)"
           },
         }
-    },
-    animation: {
-      bgAnimation: "bgAnimation .2s infinite",
-    },
+      },
+      animation: {
+        bgAnimation: "bgAnimation .2s infinite",
+      },
 
-    keyframes: {
-      float: {
+      keyframes: {
+        float: {
           '0%': {
-              transform: 'translatey(0px)'
+            transform: 'translatey(0px)'
           },
           '50%': {
-              transform: 'translatey(-30px)'
-               
+            transform: 'translatey(-30px)'
+
           },
           '100%': {
             transform: 'translatey(0px)'
-        },
-      }
-  },
-  animation: {
-      float: 'float 6s ease-in-out infinite',
-  },
+          },
+        }
+      },
+      animation: {
+        float: 'float 6s ease-in-out infinite',
+      },
     },
   },
   plugins: [
